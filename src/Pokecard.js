f6c67c4ba37c2e4f92c4218e308cd2ab64e2081a
bashrc-real <saurabh8c@gmail.com>
2020-05-08 22:49:38 -0700

    phase 3

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -15,7 +15,9 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title ">{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">EXP: {this.props.exp}</div>
       </div>
