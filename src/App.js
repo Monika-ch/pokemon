0e05a1073fa9aba05ec5ef3742de566d616058bf
monika <monika12b@gmail.com>
2020-05-19 23:08:33 -0700

    Fixing base path

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Route";
 import PokemonCache from "./PokemonCache";
+const baseUrl = "pokemon";
 class App extends Component {
   // COMMENT: Remove the line just below this before testing
   static pokemonCache = new PokemonCache();
   render() {
+    // Adding baseName so that the website works with github.icon
+    // see here:https://medium.com/@Dragonza/react-router-problem-with-gh-pages-c93a5e243819
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Routes />
         </div>
