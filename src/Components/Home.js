6bee1639dada97a3b179512a51596a8fce3e920d
monika <monika12b@gmail.com>
2020-05-19 22:09:44 -0700

    Home restyled

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -33,33 +33,41 @@ class Home extends Component {
             </div>
           </div>
         </header>
-        <div className="battleCard">
-          <div className="container">
+
+        <div className="container">
+          <div className="battleCard">
             <div className="row justify-content-center">
-              <div className="col-10">
+              <div className="col-8 mb-2">
                 <h5>Choose Your Battle</h5>
               </div>
               <div className="col-12">
-              <Link to="/pokemon" className="mt-3">
-                <Button outline color="danger" className="mt-4 p-3 col-7">
-                  vs Computer
-                </Button>
-              </Link>
-              <Link to="/friendgame">
-                <Button outline color="success" className="mt-4 p-3 col-7">
-                  vs A Friend
-                </Button>
-              </Link>
-              <Link to="/randomgame">
-                <Button outline color="info" className="mt-4 p-3 col-7">
-                  vs Random Opponent
-                </Button>
-              </Link>
-              <Link to="/game_rules" className="mt-2">
-                <Button outline className="mt-4 col-7 game-rules">
-                  Game Rules
-                </Button>
-              </Link>
+                <Link to="/pokemon">
+                  <Button outline color="danger" className="mt-2 p-2 col-6">
+                    vs Computer
+                  </Button>
+                </Link>
+                <br />
+                <Link to="/friendgame">
+                  <Button outline color="warning" className="mt-3 p-2 col-6">
+                    vs A Friend
+                  </Button>
+                </Link>
+                <br />
+                <Link to="/randomgame">
+                  <Button
+                    outline
+                    color="success"
+                    className="mt-3 mb-2 p-2 col-6"
+                  >
+                    vs Random Opponent
+                  </Button>
+                </Link>
+                <br />
+                <Link to="/game_rules" className="mt-2">
+                  <Button outline className="mt-3 col-6 game-rules">
+                    Game Rules
+                  </Button>
+                </Link>
               </div>
             </div>
           </div>
