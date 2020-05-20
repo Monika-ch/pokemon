641f032a9c21161bfe68a2d04ebd55cf0fe967b2
monika <monika12b@gmail.com>
2020-05-19 22:43:35 -0700

    Updates

---
+++ precache-manifest.d2707f78866d95101343aceb2a35fc8d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa91dd97241661fefc9386811a70a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f59a5ca30b2cd22378",
+    "url": "/static/css/2.f7bf1f6b.chunk.css"
+  },
+  {
+    "revision": "a2ef40e27c60617e24da",
+    "url": "/static/css/main.30d8a93a.chunk.css"
+  },
+  {
+    "revision": "e2f59a5ca30b2cd22378",
+    "url": "/static/js/2.6cb354dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6cb354dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ef40e27c60617e24da",
+    "url": "/static/js/main.c109d7d6.chunk.js"
+  },
+  {
+    "revision": "fb01dcf1aecacc333092",
+    "url": "/static/js/runtime-main.8f90365b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
