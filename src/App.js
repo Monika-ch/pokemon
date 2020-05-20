89b69652eceef403a9402926cf47bfd85e12bcf6
monika <monika12b@gmail.com>
2020-05-19 23:10:18 -0700

    Fixing base path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     // Adding baseName so that the website works with github.icon
     // see here:https://medium.com/@Dragonza/react-router-problem-with-gh-pages-c93a5e243819
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={baseUrl}>
         <div className="App">
           <Routes />
         </div>
