7277eaa912a704464538646b40d261ed9a5ad33d
monika <monika12b@gmail.com>
2020-05-19 22:44:25 -0700

    Revert "Updates"
    This reverts commit 641f032a9c21161bfe68a2d04ebd55cf0fe967b2.

---
+++ src/Components/DiscardCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Pokecard from './Pokecard'
+
+export default class DiscardCard extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <Pokecard
+                                id={this.props.id}
+                                name={this.props.name}
+                                type={this.props.type}
+                                exp={this.props.base_experience}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/Route.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Main from "./Components/Main";
+import Home from "./Components/Home";
+import FriendGame from "./Components/FriendGame";
+import RandomGame from "./Components/RandomGame";
+import GameRules from "./Components/GameRules";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/pokemon" component={Main} />
+      <Route path="/friendgame" component={FriendGame} />
+      <Route path="/randomgame" component={RandomGame} />
+      <Route path="/game_rules" component={GameRules} />
+      <Route path="/pokemon/:id" render={Main.pokemon} />
+      <Route exact path="/" component={Home} />
+    </Switch>
+  );
+}
