f3e243935ba265401ab27500f446fe7188b65ea8
monika <monika12b@gmail.com>
2020-05-19 22:59:57 -0700

    Updates

---
+++ precache-manifest.66c0064875383a9266170d2b6ff2ab7f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334b3ec82148fb0273f8dd0d7fdc43a4",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "7add8eb22b5954d26eb0",
+    "url": "/pokemon/static/css/2.041fc6bb.chunk.css"
+  },
+  {
+    "revision": "c7a5b1a62b7922b8fb0b",
+    "url": "/pokemon/static/css/main.30d8a93a.chunk.css"
+  },
+  {
+    "revision": "7add8eb22b5954d26eb0",
+    "url": "/pokemon/static/js/2.6cb354dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pokemon/static/js/2.6cb354dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a5b1a62b7922b8fb0b",
+    "url": "/pokemon/static/js/main.df17cb8e.chunk.js"
+  },
+  {
+    "revision": "e1366ef5cfb767bc3a5a",
+    "url": "/pokemon/static/js/runtime-main.2bfe477e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pokemon/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pokemon/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pokemon/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pokemon/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pokemon/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
