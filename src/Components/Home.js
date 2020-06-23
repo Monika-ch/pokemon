dbce912c6e6437d29c14d3c3f706300ebf5d402b
monika <monika12b@gmail.com>
2020-06-23 15:27:15 -0700

    animation applied to home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,18 +5,23 @@ import "../css/Home.css";
 import { connect } from "react-redux";
 import { getPokemonData } from "../redux/pokemonFetch";
 import PokemonCache from "../PokemonCache";
-import { fadeIn } from "react-animations";
+import { fadeInDown, fadeInUp } from "react-animations";
 import { StyleSheet, css } from "aphrodite";
 import Rotate from "react-reveal/Rotate";
+import Flip from "react-reveal/Flip";
 import Fade from "react-reveal/Fade";
+import Bounce from "react-reveal/Bounce";
 
-// const styles = StyleSheet.create({
-//   fadeIn: {
-//     animationName: fadeIn,
-//     animationDuration: "5s",
-//     animationIterationCount: "1",
-//   },
-// });
+const styles = StyleSheet.create({
+  fadeInDown: {
+    animationName: fadeInDown,
+    animationDuration: "2s",
+  },
+  fadeInUp: {
+    animationName: fadeInUp,
+    animationDuration: "2.5s",
+  },
+});
 
 const mapDispatchToProps = {
   getPokemonData,
@@ -37,69 +42,72 @@ class Home extends Component {
           <div className="container">
             <div className="row">
               <div className="col">
-                {/* <Fade left cascade duration={2500} delay={1000}> */}
-                {/* <h1 className={css(styles.fadeIn)}> */}
-                <h1>
-                  Ƥ
-                  <Rotate bottom duration={2500} delay={100}>
-                    <img
-                      className="image"
-                      src={process.env.PUBLIC_URL + "/pokeball.png"}
-                      height="70"
-                      width="70"
-                      alt="Pokeball"
-                    />
-                  </Rotate>
-                  Ƙℯḿ
-                  <Rotate bottom duration={2500} delay={100}>
-                    <img
-                      className="image"
-                      src={process.env.PUBLIC_URL + "/pokeball.png"}
-                      height="70"
-                      width="70"
-                      alt="Pokeball"
-                    />
-                  </Rotate>
-                  ȵ
-                </h1>
-                {/* </Fade> */}
-                <h3 className="arc">..let the battle begin</h3>
+                <div className={css(styles.fadeInDown)}>
+                  {/* <h1 className={css(styles.fadeIn)}> */}
+                  <h1>
+                    Ƥ
+                    <Rotate bottom duration={4000}>
+                      <img
+                        className="image"
+                        src={process.env.PUBLIC_URL + "/pokeball.png"}
+                        height="70"
+                        width="70"
+                        alt="Pokeball"
+                      />
+                    </Rotate>
+                    Ƙℯḿ
+                    <Rotate bottom duration={4000}>
+                      <img
+                        className="image"
+                        src={process.env.PUBLIC_URL + "/pokeball.png"}
+                        height="70"
+                        width="70"
+                        alt="Pokeball"
+                      />
+                    </Rotate>
+                    ȵ
+                  </h1>
+                </div>
+                <div className={css(styles.fadeInUp)}>
+                  <h3 className="arc">..let the battle begin</h3>
+                </div>
               </div>
             </div>
           </div>
         </header>
 
-        <div className="container-fluid">
-          <div className="battleCard">
-            <div className="row justify-content-center">
-              <div className="col-md-9 col-11 mb-2">
-                <h5>Choose Your Battle</h5>
-              </div>
-              <div className="col-12">
-                <Link to="/pokemon">
-                  <button className="mt-md-1 p-2 col-md-6 col-8 all-btn btn1">
-                    vs Computer
-                  </button>
-                </Link>
-                <br />
-                <Link to="/friendgame">
-                  <button className="mt-md-3 p-2 col-md-6 col-8 all-btn btn2">
-                    vs A Friend
-                  </button>
-                </Link>
-                <br />
-                <Link to="/game_rules" className="mt-2">
-                  <button
-                    outline
-                    className="mt-md-3 col-md-6 col-8 all-btn game-rules"
-                  >
-                    <span>Game Rules</span>
-                  </button>
-                </Link>
-              </div>
+        <Fade cascade duration={1000} delay={800}>
+          <div className="container-fluid">
+            <div className="battleCard">
+              <Fade top cascade duration={1600}>
+                <div className="row justify-content-center">
+                  <div className="col-md-9 col-11 mb-2">
+                    <h5>Choose Your Battle</h5>
+                  </div>
+                  <div className="col-12">
+                    <Link to="/pokemon">
+                      <button className="mt-md-1 p-2 col-md-6 col-8 all-btn btn1">
+                        vs Computer
+                      </button>
+                    </Link>
+                    <br />
+                    <Link to="/friendgame">
+                      <button className="mt-md-3 p-2 col-md-6 col-8 all-btn btn2">
+                        vs A Friend
+                      </button>
+                    </Link>
+                    <br />
+                    <Link to="/game_rules" className="mt-2">
+                      <button className="mt-md-3 col-md-6 col-8 all-btn game-rules">
+                        <span>Game Rules</span>
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </Fade>
             </div>
           </div>
-        </div>
+        </Fade>
       </div>
     );
   }
